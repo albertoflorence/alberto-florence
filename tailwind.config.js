517cb9637af31fec1cf6602d28508e1b7403f819
albertoflorence <alberto.sabatier@gmail.com>
2023-05-03 17:39:59 -0300

    configuração de thema do tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,32 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        bgDark: '#0c151d',
+        bgDarkLight: '#171F26',
+        gray: {
+          100: '#A3ABB2',
+          200: '#575757',
+          300: '#3D3D3D',
+        },
+        black: '#19191B',
+        orange: '#d47559',
       },
+    },
+    keyframes: {
+      swipeStart: {
+        '0%': { transform: 'translateX(-40%)' },
+        '100%': { transform: 'translateX(0)' },
+      },
+      swipeEnd: {
+        '0%': { transform: 'translateX(0)' },
+        '100%': { transform: 'translateX(150%)' },
+      },
+    },
+    animation: {
+      swipeStart: 'swipeStart 0.2s ease-in-out',
+      swipeEnd: 'swipeEnd 0.2s ease-in-out forwards',
     },
   },
   plugins: [],
-}
+};
