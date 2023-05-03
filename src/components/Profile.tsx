90362adda690da178c040e6b225471648d753a5b
albertoflorence <alberto.sabatier@gmail.com>
2023-05-03 17:41:43 -0300

    component de profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,13 @@
+interface ProfileProps {
+  first: string;
+  second: string;
+}
+
+export default function Profile({ first, second }: ProfileProps) {
+  return (
+    <div className="items-center text-center text-2xl text-gray-100">
+      <p>{first}</p>
+      <p>{second}</p>
+    </div>
+  );
+}
