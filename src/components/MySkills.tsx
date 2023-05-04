b8dfa3e7c0d00877338c59d6d91eb85f74a69f9e
albertoflorence <alberto.sabatier@gmail.com>
2023-05-04 13:41:03 -0300

    change Switch to only change if another button is pressed

--- src/components/MySkills.tsx
+++ src/components/MySkills.tsx
@@ -9,11 +9,10 @@ const skills = [9, 9, 9, 9, 9, 9];
 
 export default function MySkills() {
   const [on, setOn] = useState(false);
-  const toggle = () => setOn((s) => !s);
 
   return (
     <section className="w-full max-w-3xl">
-      <Switch on={on} onChange={toggle} />
+      <Switch on={on} onChange={setOn} />
       <section className="relative flex justify-center overflow-hidden rounded-lg">
         <Grid items={projects} show={!on} />
         <Grid items={skills} show={on} />
--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 
 interface SwitchProps {
   on: boolean;
-  onChange: () => void;
+  onChange: (s: boolean) => void;
 }
 
 const itens = ['Portfolio', 'Skills'];
@@ -10,21 +10,21 @@ const itens = ['Portfolio', 'Skills'];
 export default function Switch({ on, onChange }: SwitchProps) {
   return (
     <div className="my-16 w-full">
-      <label className="flex h-28 w-full cursor-pointer gap-5 rounded-[20px] bg-white  px-5 py-4 shadow-md dark:bg-bgDarkLight">
-        <input type="checkbox" onChange={onChange} className="hidden" />
+      <div className="flex h-28 w-full cursor-pointer gap-5 rounded-[20px] bg-white px-5 py-4 shadow-md dark:bg-bgDarkLight">
         {itens.map((item, i) => (
-          <div
+          <button
+            onClick={() => (item === 'Portfolio' ? onChange(false) : onChange(true))}
             key={item}
             className={clsx(
-              on && i == 0 && 'bg-gray-50 dark:bg-bgDark',
-              !on && i == 1 && 'bg-gray-50 dark:bg-bgDark',
+              !on && i == 0 && 'bg-gray-50 dark:bg-bgDark',
+              on && i == 1 && 'bg-gray-50 dark:bg-bgDark',
               'flex h-full w-full select-none items-center justify-center rounded-[10px] duration-200 dark:text-gray-100',
             )}
           >
             {item}
-          </div>
+          </button>
         ))}
-      </label>
+      </div>
     </div>
   );
 }
