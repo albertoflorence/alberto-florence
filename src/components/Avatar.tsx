229aed0d19bf58b10aea9428e46ee2aaf5dfbdbf
albertoflorence <alberto.sabatier@gmail.com>
2023-05-06 22:45:36 -0300

    change Avatar to be more responsive

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,6 +1,6 @@
 export default function Avatar() {
   return (
-    <div className="h-52 w-52">
+    <div className="h-52 w-52 shrink-0">
       <div className="h-full w-full rounded-full bg-orange"></div>
     </div>
   );
