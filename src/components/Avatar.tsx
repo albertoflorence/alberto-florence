1c26353e5db596ae44c3a5746cd2e8dd15719ca5
albertoflorence <alberto.sabatier@gmail.com>
2023-05-08 00:48:08 -0300

    add Avatar photo

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,7 +1,18 @@
+import Image from 'next/image';
+
 export default function Avatar() {
   return (
     <div className="h-52 w-52 shrink-0">
-      <div className="h-full w-full rounded-full bg-orange"></div>
+      <div className="relative flex h-full w-full items-center justify-center overflow-hidden rounded-full border-[3px] border-orange bg-white">
+        <Image
+          src="/images/avatar.jpg"
+          width={192}
+          height={192}
+          priority
+          alt="Um retrato de um homem"
+          className="rounded-full"
+        />
+      </div>
     </div>
   );
 }
