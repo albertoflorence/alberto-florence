9284fbfc7e763175bb77399101406ef1acb151a0
albertoflorence <alberto.sabatier@gmail.com>
2023-07-25 12:57:03 -0300

    fix Showcase height

--- src/components/Showcase.tsx
+++ src/components/Showcase.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import clsx from 'clsx';
 
 import Switch from './Switch';
@@ -9,14 +9,28 @@ import Skills from './Skills';
 
 export default function MySkills() {
   const [on, setOn] = useState(false);
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    const result = document.querySelector(
+      `#showcase > div:${on ? 'last' : 'first'}-of-type`,
+    )?.clientHeight;
+    if (result) setHeight(result);
+  }, [on]);
+
+  const style = {
+    height: `${height}px`,
+  };
 
   return (
     <section className="w-full max-w-3xl overflow-hidden">
       <Switch on={on} onChange={setOn} />
       <section
+        id="showcase"
+        style={style}
         className={clsx(
           'relative flex w-[200%] items-start rounded-lg transition-transform',
-          on ? '-translate-x-1/2 max-md:h-[1000px] max-sm:h-full' : 'max-sm:h-[1400px]',
+          on && '-translate-x-1/2',
         )}
       >
         <Portfolio />
