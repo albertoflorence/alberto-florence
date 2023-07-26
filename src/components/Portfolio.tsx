e1fdd698935eb384108e9b938990df622551cf6a
albertoflorence <alberto.sabatier@gmail.com>
2023-07-26 12:21:19 -0300

    add github links to Portfolio

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import Gallery from './Gallery';
 import Modal from './Modal';
 import Play from './Icons/Play';
+import { Github } from './Icons/Github';
 
 export default function Portfolio() {
   const [open, setOpen] = React.useState(false);
@@ -17,7 +18,7 @@ export default function Portfolio() {
   return (
     <>
       <Grid
-        items={projects.map(({ image, link, title, description, video }) => (
+        items={projects.map(({ image, link, title, description, video, github }) => (
           <div key={image} className="relative h-full w-full">
             <Image
               title={title}
@@ -26,17 +27,30 @@ export default function Portfolio() {
               fill
               className="object-cover"
             />
-            <div className="absolute inset-0 flex select-none flex-col items-center justify-evenly bg-orange text-white opacity-0 shadow-zinc-600 transition-opacity text-shadow hover:opacity-90">
+            <div className="absolute inset-0 flex select-none flex-col items-center justify-evenly bg-orange text-white opacity-0 shadow-zinc-600 duration-300 text-shadow hover:opacity-90">
               <h3 className="text-2xl drop-shadow-2xl">{title}</h3>
               <p className="max-w-xs justify-center text-center">{description}</p>
               <div className="flex gap-3">
-                <a
-                  href={link}
-                  target="_blank"
-                  className="flex h-[48px] w-[48px] items-center justify-center rounded-full bg-white p-3"
-                >
-                  <LinkIcon size={24} />
-                </a>
+                {link && (
+                  <a
+                    aria-label="link"
+                    href={link}
+                    target="_blank"
+                    className="flex h-[48px] w-[48px] items-center justify-center rounded-full bg-white p-3"
+                  >
+                    <LinkIcon size={24} />
+                  </a>
+                )}
+                {github && (
+                  <a
+                    aria-label="github link"
+                    href={github}
+                    target="_blank"
+                    className="flex h-[48px] w-[48px] items-center justify-center rounded-full bg-white p-3"
+                  >
+                    <Github size={24} />
+                  </a>
+                )}
                 {video && (
                   <button
                     aria-label="assistir vídeo"
@@ -73,6 +87,7 @@ const projects = [
   {
     image: 'won-games.jpg',
     link: 'https://curso-wongames.vercel.app',
+    github: 'https://github.com/albertoflorence/react-avancado-curso',
     title: 'Won Games',
     description:
       'Projeto fullstack com integração de várias techs como: Nextjs, Styled Components, Typescript, GraphQL, Jest, Cypress',
@@ -81,7 +96,7 @@ const projects = [
     video:
       'https://dms-exp2.licdn.com/playlist/vid/D4D05AQF14Eg1amyJbQ/mp4-720p-30fp-crf28/0/1689951224727?e=1690905600&v=beta&t=SMqHSm4b242ksbmyTfzceZGrPxbAbGDXGc5so9hTzzk',
     image: 'recipes-app.jpg',
-    link: '#',
+    github: 'https://github.com/albertoflorence/recipes-app',
     title: 'Recipes App',
     description: (
       <span>
@@ -94,7 +109,7 @@ const projects = [
     video:
       'https://dms-exp2.licdn.com/playlist/vid/D4D05AQEE8D0-zKoy7w/mp4-720p-30fp-crf28/0/1688759152336?e=1690905600&v=beta&t=fQA3QII1e8Kw89MvQ6AfkTNhk9PWGN4dgB_WYDX8NMI',
     image: 'starwars.jpg',
-    link: '#',
+    github: 'https://github.com/albertoflorence/starwars',
     title: 'Star Wars',
     description: (
       <span>
@@ -106,7 +121,7 @@ const projects = [
     video:
       'https://dms-exp2.licdn.com/playlist/vid/D4D05AQHwuoZ1XUG7nw/mp4-720p-30fp-crf28/0/1682527221949?e=1690905600&v=beta&t=i__yPB5JrkXBK9gxGo-M8zEyiWWHnSIB_oq9DUBUjD4',
     image: 'pixel-art.jpg',
-    link: '#',
+    github: 'https://github.com/albertoflorence/pixels-art',
     title: 'Pixel Art',
     description: (
       <span>
@@ -118,7 +133,7 @@ const projects = [
     video:
       'https://dms-exp2.licdn.com/playlist/vid/D4D05AQG7paqE4vUW9Q/mp4-720p-30fp-crf28/0/1688060209459?e=1690905600&v=beta&t=pDXq5cw-SbY5hpBjAK6QcGmG8KMbZ_yJz3c8rnbLgzM',
     image: 'trivia.jpg',
-    link: '#',
+    github: 'https://github.com/albertoflorence/trivia',
     title: 'Trivia',
     description: (
       <span>
@@ -131,7 +146,7 @@ const projects = [
     video:
       'https://dms-exp2.licdn.com/playlist/vid/D4D05AQFXQkZWrFVjyg/mp4-720p-30fp-crf28/0/1686929809761?e=1690905600&v=beta&t=ELKu4qy9zCO9EX1H6i5xgjA-SlUsZCMJkKI3WgwjvJs',
     image: 'wallet.jpg',
-    link: '#',
+    github: 'https://github.com/albertoflorence/trybewallet',
     title: 'Trybe Wallet',
     description: (
       <span>
@@ -144,7 +159,7 @@ const projects = [
     video:
       'https://dms-exp2.licdn.com/playlist/vid/D4D05AQHOQL_HNcKIyw/mp4-720p-30fp-crf28/0/1683918495005?e=1690905600&v=beta&t=4dXRKssWXU3WBDiJMTKySlEx_J09-9ZrW1L019PBfoc',
     image: 'tryunfo.jpg',
-    link: '#',
+    github: 'https://github.com/albertoflorence/tryunfo',
     title: 'Tryunfo',
     description: (
       <span>Projeto desenvolvido no módulo de front-end da {Trybe}, feito usando React</span>
@@ -154,7 +169,7 @@ const projects = [
     video:
       'https://dms-exp2.licdn.com/playlist/vid/D4D05AQHU1mek2Dr3uA/mp4-720p-30fp-crf28/0/1684534867614?e=1690905600&v=beta&t=ivZa_duykFbsdyg7GOCauP567yYHecGxNWQ_RkLvLMk',
     image: 'tunes.jpg',
-    link: '#',
+    github: 'https://github.com/albertoflorence/trybetunes',
     title: 'Trybe Tunes',
     description: (
       <span>
@@ -179,12 +194,13 @@ const projects = [
   {
     image: 'ragnarok-mercado.jpg',
     link: 'https://ragnarok-mercado.surge.sh',
+    github: 'https://github.com/albertoflorence/ragnarok-mercado',
     title: 'Ragnarok Marketplace',
     description: 'Feito para filtrar e mostrar itens vendidos em lojinhas no Ragnarok Online',
   },
   {
     image: 'clean-api.jpg',
-    link: 'https://github.com/albertoflorence/clean-architecture-api',
+    github: 'https://github.com/albertoflorence/clean-architecture-api',
     title: 'Clean Architecture',
     description:
       'Projeto de uma Api RESTful com integração de várias techs como: Typescript, Express, MongoDB, Jest, Docker, Swagger',
